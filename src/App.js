2f6e4807412a6c3843fcd1706d6e3afe53d66b1a
Subhaji-Dutta <146566101+Subhaji-Dutta@users.noreply.github.com>
2025-05-02 14:41:23 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,25 @@
+import {BrowserRouter as Router,Routes, Route } from "react-router-dom";
+import "../src/dist/styles.css";
+import Navbar from "./components/Navbar";
+import Home from './Pages/Home';
+import Contact from "./Pages/Contact";
+import About from './Pages/About';
+import Model from './Pages/Model';
+import Team from './Pages/Team';
+
+function App() {
+  return (
+    <Router>
+      <Navbar />
+      <Routes>
+        <Route index path='/' element={<Home />} />
+        <Route exact path= '/about' element={<About />}/>
+        <Route exact path= '/models' element={<Model />}/>
+        <Route exact path= '/team' element={<Team />}/> 
+        <Route exact path= '/contact' element={<Contact />}/>
+      </Routes>
+    </Router>
+  );
+}
+
+export default App;
