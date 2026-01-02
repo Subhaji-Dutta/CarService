cd2f257ef2befc274407c308fdd630832186d8c3
Subhajit-Dutta <146566101+Subhaji-Dutta@users.noreply.github.com>
2026-01-02 13:12:43 +0530

    Update App.js
    fixed home visibility

--- src/App.js
+++ src/App.js
@@ -11,6 +11,7 @@ function App() {
   return (
     <Router>
       <Navbar />
+      <Home />
       <Routes>
         <Route path='/' element={<Home />} />
         <Route path= '/about' element={<About />}/>
